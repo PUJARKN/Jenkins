73ac84ffb4f98039b171d1078879af9b0a26f250
Kapil <48823280+PUJARKN@users.noreply.github.com>
2024-10-25 11:47:23 +0530

    Update pythonbasics1.py

--- pythonbasics1.py
+++ pythonbasics1.py
@@ -6,7 +6,6 @@ print(myfunc(21,35,35,5,45,23,2,3,4))
 
 def myfunc(*args):
     return [num for num in args if num % 2 == 0]
-#myfunc(23,24,3412,122,2,1,9)
 
 
 print(myfunc(23,23,245,123,775,54,45,2,8,4,18))
